cb70097a82847d8a58d7acb5cd45dc4906e79562
wendy munyasi <wendymunyasi@gmail.com>
2023-03-08 17:33:40 +0300

    Add filecontroller and route to posting files

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 
 const router = Router();
@@ -12,5 +13,6 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', basicAuthenticate, AuthController.getConnect);
 router.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
 router.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+router.post('/files', xTokenAuthenticate, FilesController.postUpload);
 
 export default router;
