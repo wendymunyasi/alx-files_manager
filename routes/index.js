9e1b3ec330f2c7b688fd824573d344aa63625dcb
wendy munyasi <wendymunyasi@gmail.com>
2023-03-08 17:28:57 +0300

    Add authentication endpoints and middleware

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', basicAuthenticate, AuthController.getConnect);
+router.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+router.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
 export default router;
